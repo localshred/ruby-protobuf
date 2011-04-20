454a7d3bcd1228ac36488acd8d472b41092119a8
BJ <bj@moneydesktop.com>
2011-04-20 15:23:06 -0600

    Added LoggerMethods module to pb logger class for easier inclusion into other classes

--- lib/protobuf/common/logger.rb
+++ lib/protobuf/common/logger.rb
@@ -6,13 +6,52 @@ module Protobuf
     class << self
       attr_accessor :file, :level
       
-      def instance
-        @__instance ||= (@file && @level ? new(@file, @level) : nil)
+      # One-line file/level configuration
+      def configure options
+        self.file = options[:file] if options[:file]
+        self.level = options[:level] if options[:level]
       end
       
+      # Use to reset the instance
+      def reset_device!
+        self.file = self.level = @__instance = nil
+      end
+      
+      # Singleton instance
+      def instance
+        @__instance ||= begin
+          log = nil
+          if @file and @level
+            log = new(self.file)
+            log.level = self.level
+          end
+          log
+        end
+      end
+      
+      # Stub out the log methods for Protobuf::Logger as singleton methods
       [:debug, :info, :warn, :error, :fatal, :any, :add, :log].each do |m|
-        define_method(m) do |*params|
-          instance && instance.__send__(m, *params)
+        define_method(m) do |*params, &block|
+          instance && instance.__send__(m, *params, &block)
+        end
+      end
+    end
+    
+    # 
+    # LogMethods module for log method including, e.g.:
+    # 
+    # class MyClass
+    #   include Protobuf::Logger::LogMethods
+    #   ...
+    # end
+    # 
+    # Produce a module to allow "include" in other classes to avoid
+    # cluttering the namespace of the including class with the other methods defined above
+    # 
+    module LogMethods
+      [:debug, :info, :warn, :error, :fatal, :any, :add, :log].each do |m|
+        define_method("log_#{m}") do |*params, &block|
+          Protobuf::Logger.__send__(m, *params, &block)
         end
       end
     end
--- spec/unit/common/logger_spec.rb
+++ spec/unit/common/logger_spec.rb
@@ -1,65 +1,119 @@
 require 'protobuf/common/logger'
 require 'stringio'
 
-class << Protobuf::Logger
-  def reset_instance
-    Protobuf::Logger.file = nil
-    Protobuf::Logger.level = nil
-    @__instance = nil
-  end
-end
-
 describe Protobuf::Logger do
+  
+  subject { Protobuf::Logger }
+  
   before(:each) do
-    Protobuf::Logger.reset_instance
+    Protobuf::Logger.reset_device!
     Protobuf::Logger.file = '/dev/null'
     Protobuf::Logger.level = ::Logger::INFO
   end
   
-  context 'when initializing singleton' do
+  describe '.instance' do
     
-    it 'should not create a logger if the file was not set' do
-      Protobuf::Logger.file = nil
-      Protobuf::Logger.instance.should be_nil
+    it 'doesn\'t create a logger if the file was not set' do
+      subject.file = nil
+      subject.instance.should be_nil
     end
     
-    it 'should not create a logger if the level was not set' do
-      Protobuf::Logger.level = nil
-      Protobuf::Logger.instance.should be_nil
+    it 'doesn\'t create a logger if the level was not set' do
+      subject.level = nil
+      subject.instance.should be_nil
     end
     
-    it 'should get a new instance of the logger when file and level are set' do
-      Protobuf::Logger.file.should_not be_nil
-      Protobuf::Logger.level.should_not be_nil
-      Protobuf::Logger.instance.should_not be_nil
+    it 'gets a new instance of the logger when file and level are set' do
+      subject.file.should_not be_nil
+      subject.level.should_not be_nil
+      subject.instance.should_not be_nil
     end
     
-    it 'should keep the same object from multiple calls to instance' do
-      Protobuf::Logger.instance === Protobuf::Logger.instance
+    it 'keeps the same object from multiple calls to instance' do
+      subject.instance === subject.instance
+    end
+    
+  end
+  
+  describe '.configure' do
+    before(:each) { subject.reset_device! }
+    it 'sets the file and level in one call' do
+      subject.file.should_not be
+      subject.level.should_not be
+      subject.instance.should_not be
+      subject.configure :file => 'myfile.log', :level => ::Logger::WARN
+      subject.file.should == 'myfile.log'
+      subject.level.should == ::Logger::WARN
+      subject.instance.level.should == ::Logger::WARN
+    end
+    
+  end
+  
+  describe '.reset_device!' do
+    
+    it 'resets the logger instance, file, and level' do
+      subject.instance.should be
+      subject.file.should be
+      subject.level.should be
+      subject.reset_device!
+      subject.instance.should_not be
+      subject.file.should_not be
+      subject.level.should_not be
     end
     
   end
   
   context 'when logging' do
     
-    it 'should not raise errors when log instance is nil' do
-      Protobuf::Logger.reset_instance
-      Protobuf::Logger.instance.should be_nil
+    it 'doesn\'t raise errors when log instance is nil' do
+      subject.reset_device!
+      subject.instance.should be_nil
       expect {
-        Protobuf::Logger.debug 'No errors here'
-        Protobuf::Logger.info 'No errors here'
-        Protobuf::Logger.warn 'No errors here'
-        Protobuf::Logger.error 'No errors here'
-        Protobuf::Logger.fatal 'No errors here'
-        Protobuf::Logger.add 'No errors here'
-        Protobuf::Logger.log 'No errors here'
+        subject.debug 'No errors here'
+        subject.info 'No errors here'
+        subject.warn 'No errors here'
+        subject.error 'No errors here'
+        subject.fatal 'No errors here'
+        subject.add 'No errors here'
+        subject.log 'No errors here'
       }.should_not raise_error
     end
     
-    it 'should log correctly when log instance is valid' do
-      Protobuf::Logger.instance.should_not be_nil
-      Protobuf::Logger.instance.should_receive(:info).with('Should log great')
-      Protobuf::Logger.info 'Should log great'
+    it 'logs correctly when instance is valid' do
+      subject.instance.should_not be_nil
+      subject.instance.should_receive(:info).with('Should log great')
+      subject.info 'Should log great'
+    end
+    
+  end
+  
+  describe Protobuf::Logger::LogMethods do
+    
+    context 'when included in another class' do
+      
+      before(:all) do
+        class MyTestClass
+          include Protobuf::Logger::LogMethods
+        end
+      end
+      
+      subject { MyTestClass.new }
+      
+      it 'responds to all logger methods' do
+        subject.should respond_to :log_debug
+        subject.should respond_to :log_info
+        subject.should respond_to :log_warn
+        subject.should respond_to :log_error
+        subject.should respond_to :log_fatal
+        subject.should respond_to :log_add
+        subject.should respond_to :log_log
+      end
+      
+      it 'passes all embedded log calls to Logger instance' do
+        Protobuf::Logger.instance.should_receive(:debug).with('log this')
+        subject.log_debug('log this')
+      end
+      
     end
     
   end
